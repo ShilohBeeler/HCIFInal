e17a1607829b19258a513ec282f97d0a094f9ea7
ShilohBeeler <shiloh@ou.edu>
2022-04-23 17:38:12 -0500

    Added logging functions.

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template, jsonify, request, session
-import json
 
 application = Flask(__name__)
 
@@ -9,6 +8,8 @@ application.secret_key = "whatever"
 def hello_world():
 	if not 'member_data' in session:
 		session['member_data'] = []
+	if not 'logs' in session:
+		session['logs'] = []
 	return render_template('main.html')
 
 @application.route('/add_member')
@@ -52,6 +53,28 @@ def next_member():
 def show_members():
 	members = session['member_data']
 	return jsonify(result=members)
+
+@application.route('/do_action')
+def do_action():
+	member_id = request.args.get('id', -1, type=int)
+	if member_id == -1:
+		return jsonify(result="failure")
+	logs = session['logs']
+	members = session['member_data']
+	new_log = {"text": members[member_id]['name'] + " did a thing."}
+	logs.append(new_log)
+	session['logs'] = logs
+	return jsonify(result="success")
+
+@application.route('/clear_logs')
+def clear_logs():
+	session['logs'] = []
+	return jsonify(result="success")
+
+@application.route('/show_logs')
+def show_logs():
+	logs = session['logs']
+	return jsonify(result=logs)
 
 if __name__ == '__main__':
 	application.run(host='0.0.0.0', port='80')
